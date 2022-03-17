afe32b046fd420b454db1a6e2768677136b0aa1f
Lucas Nasci <lucasnsaraujo0310@gmail.com>
2022-03-17 18:13:33 -0300

    implementing transaction modal buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
 import Modal from 'react-modal';
-import { Container } from './styles'
+import { Container, TransactionTypeContainer } from './styles'
 
 import closeImg from '../../assets/close.svg'
-
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -24,6 +25,16 @@ export function NewTransactionModal({isOpen, onRequestClose } : NewTransactionMo
                 <h2>Cadastrar transação</h2>
                 <input type="text" placeholder="Título"/>
                 <input type="number" placeholder="Valor"/>
+                <TransactionTypeContainer>
+                    <button type="button">
+                        <img src={incomeImg} alt="entrada" />
+                        <span>Entrada</span>
+                    </button>
+                    <button type="button">
+                        <img src={outcomeImg} alt="saida" />
+                        <span>Saída</span>
+                    </button>
+                </TransactionTypeContainer>
                 <input type="text" placeholder="Categoria"/>
                 <button type="submit">Cadastrar</button>
             </Container>
